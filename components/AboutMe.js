a624b4c1c64d57c7153fc20a678e6618a985228d
twabbott <twabbott@outlook.com>
2023-01-02 00:17:04 -0600

    Styled components is now working (bye-bye CSS modules)

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,22 +1,31 @@
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Image from 'next/image';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  & img.avatar {
+    max-width: 100%;
+    height: auto;
+    border-radius: 50%;
+  }
+`;
 
 const AboutMe = () => {
   return (
-    <div>
+    <Container>
       <Image
         src="/images/tom.png"
         alt="john doe avatar"
         width={150}
         height={150}
-        className={styles.img}
+        className="avatar"
       />
-      <p className={styles.p}>
+      <p>
         Hey, I am <strong>John Doe</strong>. I love coding. Lorem ipsum dolor
         sit, amet consectetur adipisicing elit. Reiciendis commodi numquam
         incidunt blanditiis quibusdam atque natus inventore sunt autem iusto.
       </p>
-    </div>
+    </Container>
   );
 };
 
