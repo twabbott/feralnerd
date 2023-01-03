20e6853dc11016e429e513a8ef9252de4ee4c766
twabbott <twabbott@outlook.com>
2023-01-02 18:52:39 -0600

    Finally got an article-wrapper to work.  Here's hoping it's not just a fluke! 🤞

--- components/Frontmatter.js
+++ components/Frontmatter.js
@@ -1,19 +1,47 @@
 import Head from 'next/head';
+import styled from 'styled-components';
+
 import Date from './Date';
 
-const Frontmatter = ({ title, keywords, excerpt, date }) => {
+const Article = styled.article`
+  margin: 0 24px;
+  max-width: 1000px;
+
+  @media screen and (min-width: 1000px) {
+    & {
+      margin: 24px auto;
+      max-width: 952px;
+    }
+  }
+
+  & .title {
+    font-size: 2.5rem;
+    font-weight: 400;
+    margin-bottom: 1rem;
+  }
+
+  & .info {
+    margin-bottom: 1rem;
+  }
+`;
+
+const Frontmatter = ({ title, keywords, excerpt, date, children }) => {
+  const titleText = `${title} - Blog Rocket`;
   return (
     <>
       <Head>
-        <title>{title} - Blog Rocket</title>
+        <title>{titleText}</title>
         {keywords && <meta name="keywords" content={keywords} />}
       </Head>
 
-      <h1>{title}</h1>
-      <p>
-        <Date date={date} />
-      </p>
-      {excerpt && <p>{excerpt}</p>}
+      <Article>
+        <h1>{title}</h1>
+        <p>
+          <Date date={date} />
+        </p>
+        {excerpt && <p>{excerpt}</p>}
+        {children}
+      </Article>
     </>
   );
 };
