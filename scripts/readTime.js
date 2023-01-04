6a583a36a5fa6aa0d362d886e76fad785c016a0d
twabbott <twabbott@outlook.com>
2023-01-04 10:21:08 -0600

    Fix a start-banner bug with readig time

--- scripts/readTime.js
+++ scripts/readTime.js
@@ -1,4 +1,9 @@
-export default function readTime(content) {
+import fs from 'fs';
+import path from 'path';
+
+const START_BANNER = '{/* BEGIN */}';
+
+function readTime(content) {
   const WPS = 275 / 60;
 
   var images = 0;
@@ -26,4 +31,18 @@ export default function readTime(content) {
   const minutes = Math.ceil(((words - imageAdjust) / WPS + imageSecs) / 60);
 
   return minutes;
+}
+
+export default function getReadTime(pathname) {
+  const filename = path.join(process.cwd(), 'pages', 'posts', pathname);
+
+  let fileContents = fs.readFileSync(filename, 'utf8');
+  const bannerPos = fileContents.indexOf(START_BANNER);
+  if (bannerPos < 0) {
+    return 0;
+  }
+
+  fileContents = fileContents.substring(bannerPos + START_BANNER.length);
+
+  return readTime(fileContents);
 }
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,22 +1,6 @@
-import fs from 'fs';
 import path from 'path';
 
-import readTime from './readTime';
-
-const START_BANNER = '`{/* BEGIN */}`';
-function getReadTime(pathname) {
-  const filename = path.join(process.cwd(), 'pages', 'posts', pathname);
-
-  let fileContents = fs.readFileSync(filename, 'utf8');
-  const bannerPos = fileContents.indexOf(START_BANNER);
-  if (bannerPos < 0) {
-    return 0;
-  }
-
-  fileContents = fileContents.substring(bannerPos + START_BANNER.length);
-
-  return readTime(fileContents);
-}
+import getReadTime from './readTime';
 
 function getFileInfo(pathname) {
   const parts = pathname.split(path.sep);
